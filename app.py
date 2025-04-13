5c930ae9a7acb6bb3da57e728704d9ba7971fd9c
xxeasty <greatshawn33@naver.com>
2025-04-13 09:17:13 +0900

    .

--- app.py
+++ app.py
@@ -7,8 +7,5 @@ hide_sidebar()
 st.title("👋 사회적 상호작용 챗봇")
 st.write("원하시는 기능을 선택해주세요.")
 
-if st.button("💬 챗봇 사용하기"):
-    st.switch_page("pages/1_chatbot")
-
 if st.button("🧠 설문 시작하기"):
     st.switch_page("pages/2_survey")
